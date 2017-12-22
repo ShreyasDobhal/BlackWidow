6e456b0da499a03f6936b3eebb2da1846a27e320
Shreyas Dobhal <shreyasdobhal@gmail.com>
2017-12-22 13:46:05 +0530

    added GUI

--- Driver.java
+++ Driver.java
@@ -1,9 +1,11 @@
 class Driver {
     public static void main (String[] args) {
+        GUI window = GUI.getGUI();
         System.out.println ("Welcome to the BlackJack table!");
         System.out.println ("Best of Luck! :) ");
         System.out.println ("");
         int purse = 100;
+        window.setPurse(purse);
         Dealer dealer = new Dealer();
         Player player = new Player(purse);
         //System.out.println("");
@@ -17,10 +19,12 @@ class Driver {
                 System.out.println ("****************");
                 dealer.initDeal();
                 System.out.println ("Dealer's value = " + dealer.getHandValue());
+                window.setDealerDisplay("Dealer's value = " + dealer.getHandValue(),false);
                 System.out.println ("Player's Hand");
                 System.out.println ("****************");
                 player.initDeal();
                 System.out.println ("Hand Value = " + player.getHandValue());
+                window.setPlayerDisplay("Hand Value = " + player.getHandValue(),false);
                 if (player.getHandValue() < dealer.getHandValue()) {
                     purse = purse - get;
                 } else if (player.getHandValue() > dealer.getHandValue()) {
@@ -30,20 +34,26 @@ class Driver {
                 }
                 player.playHand();
                 System.out.println ("Hand Value = " + player.getHandValue());
+                window.setPlayerDisplay("Hand Value = " + player.getHandValue(),false);
                 dealer.playHand();
                 System.out.println ("Dealer's value = " + dealer.getHandValue());
+                window.setDealerDisplay("Dealer's value = " + dealer.getHandValue(),false);
                 player.compareHands(dealer.getHandValue());
             } else {
                 System.out.println("");
             }
             System.out.print ("Purse = ");
             System.out.println (purse);
+            window.setPurse(purse);
             System.out.println ("");
             some = player.playAgain();
             dealer = new Dealer();
             System.out.print("\f");
+            window.setPlayerDisplay("",true);
+            window.setDealerDisplay("",true);
             //player.playAgain();
         }
         System.out.println ("See you Bro!");
+        System.exit(0);
     } //end main method
 } //end class
